c286d4e2729c15c2257cdf48f3074028d23f5a29
Kuldeep Pisda <pisdak79@gmail.com>
2018-01-27 16:50:01 +0530

    Updated

--- server/article/views.py
+++ server/article/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from gensim.summarization import summarize, keywords
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+from string import punctuation
+from bs4 import BeautifulSoup
+import requests
 
 # Create your views here.
+## Remove stopwords
+
+noise_list = set(stopwords.words('english') + list(punctuation) + ["said"])
+
+def remove_noise(text):
+	words = word_tokenize(text)
+	noise_free_words = [word.lower() for word in words if word.lower() not in noise_list]
+	output_text = ' '.join(noise_free_words)
+	return output_text
+
+def get_summary(text):
+	return summarize(text)
+
+def get_keywords(text):
+	text = remove_noise(text)
+	words = word_tokenize(text)
+	text = [word.lower() for word in words if word.lower() not in noise_list]
+	output_text = ' '.join(text)
+	return keywords(output_text, split=True, pos_filter=('NN'))
